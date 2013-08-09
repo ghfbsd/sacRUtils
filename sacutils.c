c520f5798b25295cc270c2290ede3ff89c8cfe4d
ghfbsd <ghfbsd@gmail.com>
2013-08-09 17:41:09 +0100

    Update copyright information.

--- sacutils.c
+++ sacutils.c
@@ -30,6 +30,23 @@
    G. Helffrich/U. Bristol
       17 June 2012
       Revised 8 Aug. 2013
+
+   Copyright (c) 2013 by G. Helffrich.
+   All rights reserved.
+ 
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.  
+    * The names of the authors may not be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+   THERE IS NO WARRANTY EXPRESSED OR IMPLIED, NOR LIABILITY FOR DAMAGES ASSUMED,
+   IN PROVIDING THIS SOFTWARE.
+
 */
 #include <unistd.h>
 #include <sys/stat.h>
